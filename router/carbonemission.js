3c420c0bd5dce6ab05cb84be5f54cf8ec2540445
naman-verma-01 <namanvermaca@gmail.com>
2023-02-11 14:12:33 +0530

    getCurrentTab and update webpage to current

--- router/carbonemission.js
+++ router/carbonemission.js
@@ -6,74 +6,110 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post(
-    '/postdata',
-    async(req, res) => {
-        console.log(req.body);
-        let {userid,webpage,datatransferredingb}=req.body;
-        let count=0,totaldata=0;
-        let website = await Carbon.find({userid:userid,webpage:webpage});
-        console.log(website.length);
-        console.log(website);
-        if(website.length>0){
-            console.log(website[website.length-1]);
-            count=parseInt(website[website.length-1].count);
-            totaldata=parseInt(website[website.length-1].totaldata);
-        }
-//         await Carbon.find({userid:userid,webpage:webpage})
-//   .sort({date: -1})
-//   .limit(1)
-//   .exec(function(err, result) {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//         count=parseInt(result[0].count);
-//         totaldata=parseInt(result[0].totaldata);
-//       console.log(result[0]);
-//     }
-//   });
-        count=count+1;
-        totaldata=totaldata+parseInt(datatransferredingb);
-        let carbonemission=parseInt(datatransferredingb)*11;
-        let status=((carbonemission<11)?2:((carbonemission<20)?1:0));
-        console.log(status);
-        let carbondata;
+  '/postdata',
+  async (req, res) => {
+    console.log(req.body);
+    let { userid, webpage, datatransferredingb } = req.body;
+    let count = 0, totaldata = 0;
+    let website = await Carbon.find({ userid: userid, webpage: webpage });
+    console.log(website.length);
+    console.log(website);
+    if (website.length > 0) {
+      console.log(website[website.length - 1]);
+      count = parseInt(website[website.length - 1].count);
+      totaldata = parseInt(website[website.length - 1].totaldata);
+    }
+    //         await Carbon.find({userid:userid,webpage:webpage})
+    //   .sort({date: -1})
+    //   .limit(1)
+    //   .exec(function(err, result) {
+    //     if (err) {
+    //       console.log(err);
+    //     } else {
+    //         count=parseInt(result[0].count);
+    //         totaldata=parseInt(result[0].totaldata);
+    //       console.log(result[0]);
+    //     }
+    //   });
+    count = count + 1;
+    totaldata = totaldata + parseInt(datatransferredingb);
+    let carbonemission = parseInt(datatransferredingb) * 11;
+    let status = ((carbonemission < 11) ? 2 : ((carbonemission < 20) ? 1 : 0));
+    console.log(status);
+    let carbondata;
     //   try{
-       carbondata = await Carbon.create({
-            userid:userid,
-            webpage:webpage,
-            datatransferredingb:datatransferredingb,
-            totaldata:totaldata,
-            carbonemission:carbonemission,
-            status: status,
-            count:count
+    carbondata = await Carbon.create({
+      userid: userid,
+      webpage: webpage,
+      datatransferredingb: datatransferredingb,
+      totaldata: totaldata,
+      carbonemission: carbonemission,
+      status: status,
+      count: count
+    })
+
+
+    let currentWebsite = await Carbon.find({ userid: userid, webpage: "current" });
+    console.log("currentWebsite.length", currentWebsite.length, datatransferredingb)
+    if (currentWebsite.length > 0) {
+
+      let updated = await Carbon.findOneAndUpdate({ userid: userid, webpage: "current" }, { datatransferredingb: datatransferredingb, carbonemission: carbonemission,status:webpage })
+      console.log("updated", updated)
+    } else {
+      Carbon.create({
+        userid: userid,
+        webpage: "current",
+        datatransferredingb: datatransferredingb,
+        totaldata: totaldata,
+        carbonemission: carbonemission,
+        status: webpage,
+        count: count
       })
-      res.json({carbondata});
+    }
+    res.json({ carbondata });
     // }
     // catch(error){
     //     res.status(400).json({error:"there has a error in your code."});
     // }
-    },
-  );
+  },
+);
 
-  router.get('/getdata/:userid',
-    async(req,res)=>{
-          console.log(req.params);
-          let userid=req.params.userid;
-          Carbon.find({ userid: userid }, (err, results) => {
-            if (err) {
-                res.status(400).json({error:"there has a error in your code."});
-            } else {
-              console.log(results);
-              res.status(200).json({results});
-            }
-          });
-    }
-  );
-  router.get('/getalldata',
-    async(req,res)=>{
-          let alldata=await Carbon.find().sort({"totaldata":-1});
-         console.log(alldata);
-         res.status(200).json({alldata});
-    }
-  );
-  module.exports = router;
+router.get('/getdata/:userid',
+  async (req, res) => {
+    console.log(req.params);
+    let userid = req.params.userid;
+    Carbon.find({ userid: userid }, (err, results) => {
+      if (err) {
+        res.status(400).json({ error: "there has a error in your code." });
+      } else {
+        console.log(results);
+        res.status(200).json({ results });
+      }
+    });
+  }
+);
+router.get('/getalldata',
+  async (req, res) => {
+    let alldata = await Carbon.find().sort({ "totaldata": -1 });
+    console.log(alldata);
+    res.status(200).json({ alldata });
+  }
+);
+
+
+router.get('/getCurrentTab/:userid',
+  async (req, res) => {
+    
+    let userid = req.params.userid;
+    
+    Carbon.find({ userid: userid, webpage:"current" }, (err, results) => {
+      if (err) {
+        res.status(400).json({ error: "there has a error in your code." });
+      } else {
+        console.log(results);
+        res.status(200).json({ results });
+      }
+    });
+  }
+);
+module.exports = router;
